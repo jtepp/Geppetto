3ede881af15780d281144000b1e0c357a6d175ec
Jacob Tepperman <jtepp@icloud.com>
2021-03-14 01:10:58 -0500

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jacob Tepperman on 2021-03-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    init() {
+        UITextView.appearance().backgroundColor = .clear
+    }
+    @State var showSettings = false
+    @State var text = ""
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Geppetto")
+                    .font(.system(size: 50, weight: .bold, design: .rounded))
+                    .bold()
+                Spacer()
+            }
+            .padding()
+            .padding(.bottom, 100)
+            
+            TextEditor(text: $text)
+                .frame(maxWidth: UIScreen.main.bounds.width-80,
+                       minHeight: 100,
+                       idealHeight: 200,
+                       maxHeight: UIScreen.main.bounds.height-500, alignment: .topLeading)
+                .padding()
+                .background(
+                    Color("silver")
+                )
+                .cornerRadius(25)
+                .overlay(
+                    VStack {
+                        Spacer()
+                        HStack{
+                            Spacer()
+                            Button(action: openapi, label: {
+                                Image(systemName: "paperplane.fill")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(
+                                        Circle()
+                                            .fill(
+                                                Color.black
+                                            )
+                                    )
+                            })
+                            .shadow(radius: 10)
+                        }
+                    }
+                    .padding()
+                )
+                .overlay(
+            VStack {
+                HStack{
+                    Text(text.isEmpty ? "Type to GPT-3..." : "")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding()
+            .padding(.vertical, 8)
+            .padding(.horizontal, 8)
+            )
+            Spacer()
+            Button(action: {
+            showSettings = true
+            }, label: {
+            Text("Setttings")
+            .foregroundColor(.white)
+            .bold()
+            })
+            .frame(width: UIScreen.main.bounds.width-80)
+            .padding()
+            .background(
+            RoundedRectangle(cornerRadius: 25)
+            .fill(
+            Color("settingsbutton")
+            )
+            )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
+
+func openapi(){
+    print(true)
+}
---
+++ Shared/GeppettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeppettoApp.swift
+//  Shared
+//
+//  Created by Jacob Tepperman on 2021-03-14.
+//
+
+import SwiftUI
+
+@main
+struct GeppettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
