687e6c2bf6f306b988f48ad13b844d8c3846ab63
Jacob Tepperman <jtepp@icloud.com>
2021-03-14 03:28:17 -0400

    Added post function

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -40,7 +40,7 @@ struct ContentView: View {
                         Spacer()
                         HStack{
                             Spacer()
-                            Button(action: openapi, label: {
+                            Button(action: {openapi(prompt: $text)}, label: {
                                 Image(systemName: "paperplane.fill")
                                     .foregroundColor(.white)
                                     .padding()
@@ -59,9 +59,11 @@ struct ContentView: View {
                 .overlay(
             VStack {
                 HStack{
-                    Text(text.isEmpty ? "Type to GPT-3:" : "")
+                    if text.isEmpty {
+                    Text("Type to GPT-3")
                         .foregroundColor(.secondary)
                         .offset(y:-60)
+                    }
                     Spacer()
                 }
                 Spacer()
@@ -100,6 +102,44 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-func openapi(){
-    print(true)
+func openapi(prompt: Binding<String>){
+    let session = URLSession(configuration: .default)
+    let url = URL(string: "https://api.openai.com/v1/engines/davinci/completions")
+    var request = URLRequest(url: url!)
+    do {
+        let json : [String:Any] = [
+            "prompt":prompt.wrappedValue,
+            "temperature":0,
+            "top_p":1,
+            "frequency_penalty":0.0,
+            "presence_penalty":0.0,
+            "stop":["###"]
+        ]
+    let data = try JSONSerialization.data(withJSONObject: json, options: [])
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.addValue("Bearer \(UserDefaults.standard.string(forKey: "key") ?? "")", forHTTPHeaderField: "Authorization")
+    request.httpBody = data
+    let task = session.dataTask(with: request) { data, response, error in
+        // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+         
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    
+                    print("Response data string:\n \(dataString)")
+                    
+                    if dataString.contains("You didn't provide an API key") {
+                        
+                    }
+                }
+    }
+    task.resume() // <- otherwise your network request won't be started
+    } catch {
+        print("ERROR")
+    }
 }
